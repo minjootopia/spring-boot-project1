f95ee0da5a690ab921421eb874e12e62ac3880e3
minjoo <minjoook99@gmail.com>
2025-11-24 16:44:13 +0900

    rename 2차

--- src/test/java/com/kdt11/SpringBootJPA.java
+++ src/test/java/com/kdt11/SpringBootJPA.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringStarter251121ApplicationTests {
+class SpringBootJPAApplicationTests {
 
 	@Test
 	void contextLoads() {
